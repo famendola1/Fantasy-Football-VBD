96e50d1220b744351d45f6712e19c664b75c46fb
famendola1 <fabio.amendola20@gmail.com>
2017-08-16 12:31:04 -0400

    Added main function

--- vbd.py
+++ vbd.py
@@ -4,8 +4,6 @@ import numpy as np
 file = "ffa_customrankings2017-0.csv"
 
 draft_size = 272 # 16 teams, 17 players per team
-all_projections = pd.read_csv(file)
-projections = all_projections.head(draft_size)
 
 # Value Over Replacement Player (VORP)
 
@@ -32,99 +30,102 @@ def adjust(row, pos, mult):
         row[3] *= mult
     return row
 
-all_positions = ['QB', 'RB', 'WR', 'TE', 'DST', 'K']
+if __name__ == "__main__":
+    all_projections = pd.read_csv(file)
+    projections = all_projections.head(draft_size)
 
-replacement_qb = 0
-replacement_rb = 0
-replacement_wr = 0
-replacement_te = 0
-replacement_dst = 0
-replacement_k = 0
+    all_positions = ['QB', 'RB', 'WR', 'TE', 'DST', 'K']
 
-for pos in all_positions:
-    # Query the position players to find the last ranked at that position
-    # that would be drafted
-    expression = "playerposition == '" + pos +"'"
-    draft_pos = projections.query(expression)
+    replacement_qb = 0
+    replacement_rb = 0
+    replacement_wr = 0
+    replacement_te = 0
+    replacement_dst = 0
+    replacement_k = 0
 
-    # Get the last rank
-    draft_pos = draft_pos.sort_values(by='positionRank')
-    last_rank = draft_pos.tail(1)['positionRank'].iloc[0]
+    for pos in all_positions:
+        # Query the position players to find the last ranked at that position
+        # that would be drafted
+        expression = "playerposition == '" + pos +"'"
+        draft_pos = projections.query(expression)
 
-    # Get the projected points of the first replacement player
-    expression = "positionRank > " + str(last_rank)
-    replacement_players = all_projections.query(expression)
-    replacement_players = replacement_players.sort_values(by='positionRank')
-    replacement_points = replacement_players.head(1)['points'].iloc[0]
+        # Get the last rank
+        draft_pos = draft_pos.sort_values(by='positionRank')
+        last_rank = draft_pos.tail(1)['positionRank'].iloc[0]
 
-    if pos == 'QB':
-        replacement_qb = replacement_points
-    elif pos == 'RB':
-        replacement_rb = replacement_points
-    elif pos == 'WR':
-        replacement_wr = replacement_points
-    elif pos == 'TE':
-        replacement_te = replacement_points
-    elif pos == 'DST':
-        replacement_dst = replacement_points
-    elif pos == 'K':
-        replacement_k = replacement_points
+        # Get the projected points of the first replacement player
+        expression = "positionRank > " + str(last_rank)
+        replacement_players = all_projections.query(expression)
+        replacement_players = replacement_players.sort_values(by='positionRank')
+        replacement_points = replacement_players.head(1)['points'].iloc[0]
 
-# Extract only the relevant columns
-projections = projections[["player", "playerposition", "points"]]
+        if pos == 'QB':
+            replacement_qb = replacement_points
+        elif pos == 'RB':
+            replacement_rb = replacement_points
+        elif pos == 'WR':
+            replacement_wr = replacement_points
+        elif pos == 'TE':
+            replacement_te = replacement_points
+        elif pos == 'DST':
+            replacement_dst = replacement_points
+        elif pos == 'K':
+            replacement_k = replacement_points
 
-projections["vbd"] = 0
-projections = projections.apply(func=set_vbd, axis=1, broadcast=True)
-projections = projections.sort_values(by="vbd", ascending=False)
-projections.to_csv("original.csv")
+    # Extract only the relevant columns
+    projections = projections[["player", "playerposition", "points"]]
 
-while True:
-    print("What would you like to do?")
-    print("-- Type 'remove' to remove a player")
-    print("-- Type 'adjust' to adjust VBD")
-    print("-- Type 'draft' to display the player to draft")
-    print("-- Type 'display' to show the top 10 players available")
-    print("-- Type 'exit' to leave")
+    projections["vbd"] = 0
+    projections = projections.apply(func=set_vbd, axis=1, broadcast=True)
+    projections = projections.sort_values(by="vbd", ascending=False)
+    projections.to_csv("original.csv")
 
-    choice = input()
-    print()
+    while True:
+        print("What would you like to do?")
+        print("-- Type 'remove' to remove a player")
+        print("-- Type 'adjust' to adjust VBD")
+        print("-- Type 'draft' to display the player to draft")
+        print("-- Type 'display' to show the top 10 players available")
+        print("-- Type 'exit' to leave")
 
-    if choice == 'remove':
-        name = input("Name?: ")
-        pos = input("Position?: ")
-        expression = "player != '" + name + "'"
-        projections.query(expr=expression, inplace=True)
-        print(name + " was removed")
+        choice = input()
         print()
-    elif choice == 'adjust':
-        pos = input("Position?: ")
-        mult = float(input("Multiplier?: "))
 
-        projections = projections.apply(func=adjust, axis=1, args=(pos, mult), broadcast=True)
-        projections.sort_values(by="vbd", inplace=True, ascending=False)
-        projections.to_csv("updated.csv")
-        print("VBD was adjusted")
-        print()
-    elif choice == 'draft':
-        pos = input("Position? (ANY, QB, WR, RB, DST, K, TE): ")
-        print()
-        if pos == 'ANY':
-            print(projections.head(1))
-        else:
-            expression = "playerposition == '" + pos + "'"
-            players = projections.query(expression)
-            print(players.head(1))
-        print()
-    elif choice == 'display':
-        pos = input("Position? (ALL, QB, WR, RB, DST, K, TE): ")
-        print()
-        if pos == 'ALL':
-            print(projections.head(10))
-        else:
-            expression = "playerposition == '" + pos + "'"
-            players = projections.query(expression)
-            print(players.head(10))
-        print()
-    elif choice == 'exit':
-        print("Good Luck This Season :)")
-        break
+        if choice == 'remove':
+            name = input("Name?: ")
+            expression = "player != '" + name + "'"
+            projections.query(expr=expression, inplace=True)
+            print(name + " was removed")
+            print()
+        elif choice == 'adjust':
+            pos = input("Position? (QB, RB, WR, TE, DST, K): ")
+            mult = float(input("Multiplier?: "))
+
+            projections = projections.apply(func=adjust, axis=1, args=(pos, mult), broadcast=True)
+            projections.sort_values(by="vbd", inplace=True, ascending=False)
+            projections.to_csv("updated.csv")
+            print("VBD was adjusted")
+            print()
+        elif choice == 'draft':
+            pos = input("Position? (ANY, QB, RB, WR, TE, DST, K): ")
+            print()
+            if pos == 'ANY':
+                print(projections.head(1))
+            else:
+                expression = "playerposition == '" + pos + "'"
+                players = projections.query(expression)
+                print(players.head(1))
+            print()
+        elif choice == 'display':
+            pos = input("Position? (ALL, QB, RB, WR, TE, DST, K): ")
+            print()
+            if pos == 'ALL':
+                print(projections.head(10))
+            else:
+                expression = "playerposition == '" + pos + "'"
+                players = projections.query(expression)
+                print(players.head(10))
+            print()
+        elif choice == 'exit':
+            print("Good Luck This Season :)")
+            break
